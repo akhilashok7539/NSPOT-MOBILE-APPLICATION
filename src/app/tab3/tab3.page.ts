b86d79de4ac01a36f8257c60be081dd6a5a0d671
akhilashok7539 <akhilashok7539@gmail.com>
2021-04-11 22:16:49 +0530

    changes

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
+import { endPoints } from '../_config/endpoints';
+import { InstituteService } from '../_services/institute.service';
 
 @Component({
   selector: 'app-tab3',
@@ -8,14 +10,24 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page implements OnInit{
-
-  constructor(private router:Router,private menu:MenuController) {}
+  instituteInfo:any = [];
+  instituteId;
+  institutedetails:any = [];
+    constructor(private router:Router,private menu:MenuController,private apiservice:InstituteService) 
+    {
+    
+    }
   logout(){
     localStorage.clear();
     sessionStorage.clear();
     this.router.navigate([''])
   }
-
+  ionViewWillEnter() {
+    this.institutedetails = JSON.parse(localStorage.getItem('userLogin'));
+    console.log(this.institutedetails['userProfile'].userType_modelId);
+    this.instituteId = this.institutedetails['userProfile'].userType_modelId;
+    this.loadInstituteInfo();
+  }
   ngOnInit() {
     this.menu.enable(true);
     this.menu.swipeGesture(true);
@@ -36,5 +48,15 @@ export class Tab3Page implements OnInit{
 
   }
 
+  loadInstituteInfo() {
+    // fetching institute details
+    this.apiservice.doGetRequest(endPoints.GetInstituteInfo + this.instituteId).subscribe((returnData: any) => {
+      console.log(returnData)
+      this.instituteInfo = returnData.data;
+    }, error => {
+      console.error(error);
+      // this.toastr.error('Failed to fetch institute details')
+    });
+  }
 
 }
